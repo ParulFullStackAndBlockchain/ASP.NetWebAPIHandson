0cdc9761fa7cf856f03419c5b29acc3d2e26f395
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-03 12:09:24 +0530

    ASP.NET Web API and SQL Server

---
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (WebAPIDemoEmployeeDBEntities entities = new WebAPIDemoEmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (WebAPIDemoEmployeeDBEntities entities = new WebAPIDemoEmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
