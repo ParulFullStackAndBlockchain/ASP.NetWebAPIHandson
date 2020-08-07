b3103a55144e9dc8a59465c4b45f1d74a1028619
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-07 11:29:01 +0530

    Implementing post method in ASP.NET Web API

--- WebAPIDemo/App_Start/WebApiConfig.cs
+++ WebAPIDemo/App_Start/WebApiConfig.cs
@@ -12,23 +12,7 @@ namespace WebAPIDemo
 {
     public static class WebApiConfig
     {
-        //To return JSON instead of XML from ASP.NET Web API Service when a request is made from the browser.
-        //But When the request is issued from a tool like fiddler the Accept header value should be respected. 
-        //Approach-2
-        public class CustomJsonFormatter : JsonMediaTypeFormatter
-        {
-            public CustomJsonFormatter()
-            {
-                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-            }
-
-            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
-            {
-                base.SetDefaultContentHeaders(type, headers, mediaType);
-                headers.ContentType = new MediaTypeHeaderValue("application/json");
-            }
-        }
-
+        
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -43,23 +27,7 @@ namespace WebAPIDemo
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
-
-            //To return only JSON from ASP.NET Web API Service irrespective of the Accept header value
-            //config.Formatters.Remove(config.Formatters.XmlFormatter);
-
-            //To return only XML from ASP.NET Web API Service irrespective of the Accept header value
-            //config.Formatters.Remove(config.Formatters.JsonFormatter);
-
-            //To return JSON instead of XML from ASP.NET Web API Service when a request is made from the browser.
-            //Approach-1
-            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-
-            //To return JSON instead of XML from ASP.NET Web API Service when a request is made from the browser.
-            //But When the request is issued from a tool like fiddler the Accept header value should be respected. 
-            //Approach-2
-            config.Formatters.Add(new CustomJsonFormatter());//Registers the formatter
-
+            );            
         }
     }
 }
