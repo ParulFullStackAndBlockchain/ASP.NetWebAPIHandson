66b7107d45a32b02bc4e1ddba2b57cda72104e9e
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-11 11:12:45 +0530

    ASP.NET Web API query string parameters

--- WebAPIDemo/Controllers/EmployeesController.cs
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -10,42 +10,30 @@ namespace WebAPIDemo.Controllers
 {
     public class EmployeesController : ApiController
     {
-        //By default, the HTTP verb GET is mapped to a method in a controller that has the name Get() or starts with the word Get.
-        //If a method is named Get(),by convention it is mapped to the HTTP verb GET. 
-        //Even if you rename it to GetEmployees() or GetSomething() it will still be mapped to the HTTP verb GET as long as the 
-        //name of the method is prefixed with the word Get. The word Get is case-insensitive. 
-        //It can be lowercase, uppercase or a mix of both.
-
-        //If the method is not named Get or if it does not start with the word get then Web API does not know the method name to 
-        //which the GET request must be mapped and the request fails with an error message stating The requested resource does not
-        //support http method 'GET' with the status code 405 Method Not Allowed. Now if we rename Get() 
-        //method to LoadEmployees() and issue a GET request, the request will fail, because ASP.NET Web API does not know it 
-        //has to map the GET request to this method.
-
-        //To instruct Web API to map HTTP verb GET to LoadEmployees() method, decorate the method with [HttpGet] attribute.
-
-        //If we add another method as LoadEmployeeById(int id). 
-
-        //When we navigate to the following URI, notice we are getting all the Employees, instead of just the Employee with Id=1. 
-        //This is because in this case the GET request is mapped to LoadEmployees() and not LoadEmployeeById(int id). If you want 
-        //the GET request to be mapped to LoadEmployeeById(int id) when the id parameter is specified in the URI, decorate 
-        //LoadEmployeeById(int id) method also with [HttpGet] attribute. http://localhost:3567/api/Employees/1
-
-        //Attributes that are used to map your custom named methods in the controller class to GET, POST, PUT and DELETE http verbs
-        //are [HttpGet], [HttpPost],[HttpPut]and [HttpDelete] respetively.
-
-
-        [HttpGet]
-        public IEnumerable<Employee> LoadEmployees()
+        // gender parameter is mapped to the gender parameter sent in the query string    
+        public HttpResponseMessage Get(string gender = "All")
         {
             using (WebAPIDemoEmployeeDBEntities entities = new WebAPIDemoEmployeeDBEntities())
             {
-                return entities.Employees.ToList();
+                switch (gender.ToLower())
+                {
+                    case "all":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.ToList());
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+                    default:
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                            "Value for gender must be Male, Female or All. " + gender + " is invalid.");
+                }
             }
         }
 
-        [HttpGet]
-        public HttpResponseMessage LoadEmployeeById(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (WebAPIDemoEmployeeDBEntities entities = new WebAPIDemoEmployeeDBEntities())
             {
