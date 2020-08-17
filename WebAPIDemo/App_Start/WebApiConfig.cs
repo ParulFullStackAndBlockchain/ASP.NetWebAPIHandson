5fe420be8f87feeefe7aa63fe39085ffff528100
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-17 18:18:01 +0530

    Attribute routing in ASP.NET Web API 2

--- WebAPIDemo/App_Start/WebApiConfig.cs
+++ WebAPIDemo/App_Start/WebApiConfig.cs
@@ -22,9 +22,12 @@ namespace WebAPIDemo
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
-            // Web API routes
+            //How to enable Attribute Routing?
+            //In ASP.NET Web API 2, Attribute Routing is enabled by default using following line of code.
             config.MapHttpAttributeRoutes();
 
+            //Convention-based routing : When we create a new Web API project using Visual Studio, 
+            //a default route is created as shown below.
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
@@ -38,13 +41,4 @@ namespace WebAPIDemo
         }
     }
 }
-
-//Enable basic authentication
-//1. The BasicAuthenticationAttribute can be applied on a specific controller, specific action, or globally on all Web API 
-//controllers.
-//2. To enable basic authentication across the entire Web API application, register BasicAuthenticationAttribute as a filter 
-//using the Register() method in WebApiConfig class.
-//config.Filters.Add(new RequireHttpsAttribute());
-//3. You can also apply the attribute on a specific controller, to enable basic authentication for all the methods in that 
-//controller
 
---
+++ WebAPIDemo/Controllers/StudentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "Tom" },
+            new Student() { Id = 2, Name = "Sam" },
+            new Student() { Id = 3, Name = "John" }
+        };
+
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        //Can we use both Attribute Routing and Convention-based routing in a single Web API project ?
+        //Yes, both the routing mechanisms can be combined in a single Web API project.The controller action methods that have 
+        //the[Route] attribute uses Attribute Routing, and the others without[Route] attribute uses Convention-based routing.
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        //What is Attribute Routing?
+        //Using the[Route] attribute to define routes is called Attribute Routing
+
+        //What are the advantages of using Attribute Routing?
+        //Attribute routing gives us more control over the URIs than convention-based routing.Creating URI patterns like hierarchies of
+        //resources (For example, students have courses, Departments have employees) is very difficult with convention-based routing.
+        //With attribute routing all you have to do is use the [Route] attribute as shown below.
+        [Route("api/students/{id}/courses")]
+        public IEnumerable<string> GetStudentCourses(int id)
+        {
+            if (id == 1)
+                return new List<string>() { "C#", "ASP.NET", "SQL Server" };
+            else if (id == 2)
+                return new List<string>() { "ASP.NET Web API", "C#", "SQL Server" };
+            else
+                return new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+        }
+    }
+}
